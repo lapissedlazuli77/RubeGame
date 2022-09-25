8091c515dce2e10a7ad7fcc9ec8311028cfa4527
William Suwandi <ws2315@nyu.edu>
2022-09-25 14:11:50 -0400

    Working on further stages; still need to work on the camera following the machine as things collide. Currently wanting to work out how to transfer camera focus from one object to another

--- Assets/Scripts/FocusedMovement.cs
+++ Assets/Scripts/FocusedMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FocusedMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform targetFocus;
+
     void Start()
     {
         
@@ -13,6 +15,10 @@ public class FocusedMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(targetFocus.gameObject.transform.currentfocus != true)
+        {
+
+        }
+        this.transform.position = new Vector3(targetFocus.position.x, targetFocus.position.y, this.transform.position.z);
     }
 }
